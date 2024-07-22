205dd11832f8c10711604ebf940eb8ac31271c7f
Aastha Sangani <111308524+aastha-5103@users.noreply.github.com>
2024-07-22 12:59:05 -0400

    still developing

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datatime as dt
+
+
+from sklearn.preprocessing import MinMaxScalar
+from tenserflow.keras.models import Sequential
+from tenserflow.keras
